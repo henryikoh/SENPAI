0e20b590ce56da44952f57a1c12f18682f306d7a
Henry Ikoh <think.senpai@gmail.com>
2020-01-07 22:14:43 +0100

    new site

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
 	/*
 	 ** Headers of the page
 	 */
-	mode: 'spa',
+	mode: 'universal',
 	head: {
 		title: 'SENPAI | A Social Impact Design Company in Lagos, Nigeria',
 		meta: [
@@ -122,22 +122,7 @@ export default {
 		/*
 		 ** You can extend webpack config here
 		 */
-		extend (config, ctx) {
-      if (ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    },
-
-    postcss: [
-      require('autoprefixer')({
-        browsers: ['> 5%']
-      })
-    ]
+		
 	},
 	vue: {
 		config: {

a6563671cf7e500b2b33cc8d2e31ae8f37fd64ea
Henry Ikoh <think.senpai@gmail.com>
2020-05-01 03:26:19 +0100

    adding senpai academy

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,8 +3,6 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _314dc354 = () => interopDefault(import('../pages/academy/index.vue' /* webpackChunkName: "pages/academy/index" */))
-const _10632842 = () => interopDefault(import('../pages/academy/courses/design101.vue' /* webpackChunkName: "pages/academy/courses/design101" */))
 const _67e433b7 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 const _1f058e06 = () => interopDefault(import('../pages/_category/_slug.vue' /* webpackChunkName: "pages/_category/_slug" */))
 
@@ -25,14 +23,6 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
-    path: "/academy",
-    component: _314dc354,
-    name: "academy"
-  }, {
-    path: "/academy/courses/design101",
-    component: _10632842,
-    name: "academy-courses-design101"
-  }, {
     path: "/",
     component: _67e433b7,
     name: "index"
